2a235382a8abe3014589596cc80d3cf6635d2104
Desmund <dennymauladinoor23@gmail.com>
2025-09-30 12:55:59 +0800

    source code c

---
+++ C/PRAK002-2510817310012-DENNYMAULADINOOR.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+    
+    char nama [] = "Denny Mauladinoor" ;
+    printf ("selamat pagi %s\n", nama) ;
+    printf ("selamat siang %s\n", nama) ;
+    printf ("selamat malam %s\n", nama) ;
+    
+    return 0 ;
+}
---
+++ C/PRAK004-2510817310012-DENNYMAULADINOOR.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+    printf("####################################\n");
+    printf("#                                  #\n");
+    printf("#        Denny Mauladinoor         #\n");
+    printf("#           2510817310012          #\n");
+    printf("#                                  #\n");
+    printf("####################################\n");
+    return 0 ;
+}
